bf0ad93a797ae1f4e28804272f0b555bd197a6c7
Aryan Raju Bankar <aryanbankarcollege@gmail.com>
2026-08-18 02:12:30 +0530

    final-push

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
 import { Experience } from "@/app/components/crypto/experience";
-import { Hero } from "@/app/components/Hero";
 
 export default function Page() {
-  return (
-    <main>
-      <Hero />
-      <Experience />
-    </main>
-  );
+  return <Experience />;
 }
